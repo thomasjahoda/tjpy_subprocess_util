069592dfdf847d0ddaca1bfa4ad7811c333997a4
Thomas Jahoda <thomasjahoda@users.noreply.github.com>
2019-04-03 23:36:06 +0200

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     packages=find_packages(include=['tjpy_subprocess_util', 'tjpy_subprocess_util.*']),
     test_suite='tests',
     url='https://github.com/thomasjahoda/tjpy_subprocess_util',
-    version='0.1.1',
+    version='0.1.2',
     zip_safe=False,
 )
